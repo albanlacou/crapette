bc17a90be4d3c61537666aa39ece454819273c4e
Alban642 <66842827+Alban642@users.noreply.github.com>
2021-09-20 22:20:00 +0200

    initalisation du projet et terrain de jeu

---
+++ main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Crapette en ligne</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="ennemie">
+		<div class="ennemieContainer">
+			<div class="defausse"></div>
+			<div class="deck"></div>
+			
+		</div>
+	</div>
+	<div class="main">
+		<div class="middle_cont">
+			<div class="pile1" id="pile"></div>
+			<div class="pile2" id="pile"></div>
+			<div class="pile3" id="pile"></div>
+			<div class="pile4" id="pile"></div>
+			<div class="pile5" id="pile"></div>
+		</div>
+		
+	</div>
+	
+	<div class="moi">
+		<div class="setContainer">
+			<div class="deck"></div>
+			<div class="defausse"></div>
+		</div>
+	</div>
+
+</body>
+</html>
